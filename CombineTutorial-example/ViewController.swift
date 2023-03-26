edfa4639e9a4d76f31b25ec08788f71dadc8ba4e
minhye9731 <jenney62639731@gmail.com>
2023-03-26 23:40:03 +0900

    feat : 메인화면 UI 구서 및 화면이동 추가

--- CombineTutorial-example/ViewController.swift
+++ CombineTutorial-example/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import CombineCocoa
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    @IBOutlet weak var navToNumbersBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navToNumbersBtn
+            .tapPublisher
+            .sink {
+                print(#fileID, #function, #line, "-")
+                
+                #warning("TODO : -numbers로 화면이동")
+                let numbersStoryboad = UIStoryboard(name: "Numbers", bundle: Bundle.main)
+                let numbersVC = numbersStoryboad.instantiateViewController(withIdentifier: "NumbersViewController")
+                self.navigationController?.pushViewController(numbersVC, animated: true)
+            }
+            .store(in: &subscriptions)
     }
 
 
